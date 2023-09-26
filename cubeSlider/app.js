bca26ff667a8231258b9388de2bbc554791ecaac
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2023-09-26 16:15:32 +0330

    update!!!!!!!!!!

--- cubeSlider/app.js
+++ cubeSlider/app.js
@@ -12,3 +12,13 @@ btnPrev.addEventListener("click", () => {
   pos += 90;
   cube.style.transform = `rotateY(${pos}deg)`;
 });
+
+
+// Mobile
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
