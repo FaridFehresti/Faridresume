75c62001b3fe46a1b54a0dff23c72fa1f53b1a49
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2023-09-26 16:42:50 +0330

    fixed contact page

--- DrumKit/app.js
+++ DrumKit/app.js
@@ -49,3 +49,11 @@ function eventAdder() {
 }
 
 eventAdder();
+//  Mobile @@@@@@@@@@@@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
